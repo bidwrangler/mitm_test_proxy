70122f30714ed32388f1294edcfe74180c0ebcd7
Aurélien Chivot-Buhler <aurelien@arimaska.net>
2025-07-29 09:57:11 +0100

    Fix socket resource leaks and infinite loops in HttpConnectApp
    - Add proper socket cleanup with ensure blocks
    - Add timeout protection for parser infinite loops
    - Add write timeouts to prevent thread blocking
    - Improve error handling for connection issues
    - Always close ssl_socket and client_socket in all code paths

--- lib/mitm_test_proxy/http_connect_app.rb
+++ lib/mitm_test_proxy/http_connect_app.rb
@@ -28,10 +28,12 @@ module MitmTestProxy
     def handle_connect(env)
       hijack = env.fetch('rack.hijack')
       client_socket = hijack.call
-      client_socket.write("HTTP/1.1 200 Connection Established\r\n\r\n")
-
-      hostname = env.fetch('REQUEST_URI').split(':').first
+      ssl_socket = nil
+      
       begin
+        client_socket.write("HTTP/1.1 200 Connection Established\r\n\r\n")
+
+        hostname = env.fetch('REQUEST_URI').split(':').first
         ssl_socket = @context_manager.setup_ssl_socket(hostname, client_socket)
 
         ssl_socket.accept
@@ -40,16 +42,23 @@ module MitmTestProxy
           parser = Puma::HttpParser.new
 
           request_env = {}
-          while !parser.finished?
+          timeout_counter = 0
+          max_parse_attempts = 100  # Prevent infinite loops
+          
+          while !parser.finished? && timeout_counter < max_parse_attempts
             begin
               buffer = ssl_socket.readpartial(1024)
             rescue EOFError
               break
+            rescue IO::TimeoutError, Errno::EAGAIN
+              timeout_counter += 1
+              next
             end
             parser.execute(request_env, buffer, 0)
+            timeout_counter += 1
           end
 
-          break if request_env.length == 0
+          break if request_env.length == 0 || timeout_counter >= max_parse_attempts
 
           request_env["REQUEST_URI"] = "https://#{hostname}#{request_env.fetch('REQUEST_URI')}"
 
@@ -59,10 +68,27 @@ module MitmTestProxy
         end
       rescue Errno::ECONNRESET => error
         # Client closed the connection
+        log("MitmTestProxy Client disconnected: #{hostname}")
       rescue => error
-        response = [500, {}, [error.message]]
         log("MitmTestProxy Error: #{error.inspect}, #{error.backtrace.join("\n")}")
-        write_response_to(ssl_socket, response)
+        begin
+          response = [500, {}, [error.message]]
+          write_response_to(ssl_socket, response) if ssl_socket
+        rescue
+          # Ignore errors when trying to write error response
+        end
+      ensure
+        # Always cleanup sockets
+        begin
+          ssl_socket&.close
+        rescue
+          # Ignore errors when closing
+        end
+        begin
+          client_socket&.close
+        rescue
+          # Ignore errors when closing  
+        end
       end
 
       [200, {}, []] # Return a successful response
@@ -74,14 +100,25 @@ module MitmTestProxy
       # Format the status line
       http_status_line = "HTTP/1.1 #{status} #{Rack::Utils::HTTP_STATUS_CODES[status]}\r\n"
 
-      socket.write(http_status_line)
-      # Format the headers
-      http_headers = headers.map { |key, value| "#{key}: #{value}\r\n" }.join
-      socket.write(http_headers)
-      socket.write("\r\n")
+      begin
+        # Set socket write timeout to prevent indefinite blocking
+        socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_SNDTIMEO, [5, 0].pack("l_2")) if socket.respond_to?(:setsockopt)
+        
+        socket.write(http_status_line)
+        # Format the headers
+        http_headers = headers.map { |key, value| "#{key}: #{value}\r\n" }.join
+        socket.write(http_headers)
+        socket.write("\r\n")
 
-      body.each do |chunk|
-        socket.write(chunk)
+        body.each do |chunk|
+          socket.write(chunk)
+        end
+      rescue Errno::EPIPE, Errno::ECONNRESET, IO::TimeoutError => e
+        log("MitmTestProxy Write error (client may have disconnected): #{e.message}")
+        # Don't re-raise, just log and continue
+      rescue => e
+        log("MitmTestProxy Unexpected write error: #{e.message}")
+        raise
       end
     end
   end
